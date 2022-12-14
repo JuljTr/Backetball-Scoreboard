b9664a5a066d3a9aad5b844a0f678c5b2bd83b09
Juljeta Troka <jj.rt@outlook.com>
2022-12-14 14:01:57 +0000

    added functionality to the buttons, score can be displayed and points can be added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,57 @@
-
+// Selecting display for points
 const homeScore = document.querySelector(".home-score");
 const guestScore = document.querySelector(".guest-score");
 
-const onePoint = document.getElementById("add-one-point");
-const twoPoints = document.getElementById("add-two-points");
-const threePoints = document.getElementById("add-three-points");
+// Selecting Buttons to add points 
+const onePointHome = document.getElementById("add-one-point");
+const twoPointsHome = document.getElementById("add-two-points");
+const threePointsHome = document.getElementById("add-three-points");
 
-let homeResult = 1;
-homeScore.textContent= homeResult;
+const onePointGuest = document.getElementById("add-one-point-guest");
+const twoPointsGuest = document.getElementById("add-two-points-guest");
+const threePointsGuest = document.getElementById("add-three-points-guest");
 
-let guestResult = 5;
-guestScore.textContent= guestResult;
+let homeResult = 0;
+let guestResult = 0;
+
+
+// Functions for adding Home points 
+function addOnePointHome() {
+    homeScore.textContent = homeResult;
+    return homeResult+= 1
+}
+
+function addTwoPointHome() {
+    homeScore.textContent = homeResult;
+    return homeResult+= 2
+}
+
+function addThreePointHome() {
+    homeScore.textContent = homeResult;
+    return homeResult+= 3
+}
+
+// Functions for adding Guest Points 
+function addOnePointGuest() {
+    guestScore.textContent = guestResult;
+    return guestResult+= 1;
+}
+function addTwoPointGuest() {
+    guestScore.textContent = guestResult;
+    return guestResult+= 2;
+}
+
+function addThreePointGuest() {
+    guestScore.textContent = guestResult;
+    return guestResult+= 3;
+}
+
+// Event Listeners for buttons 
+onePointHome.addEventListener("click",addOnePointHome);
+twoPointsHome.addEventListener("click",addTwoPointHome);
+threePointsHome.addEventListener("click",addThreePointHome);
+
+
+onePointGuest.addEventListener("click",addOnePointGuest)
+twoPointsGuest.addEventListener("click",addTwoPointGuest)
+threePointsGuest.addEventListener("click",addThreePointGuest)
