96eb1e480167ad00eae3cf139d5ee268591a3798
Juljeta Troka <jj.rt@outlook.com>
2022-12-14 13:40:18 +0000

    selected buttons and elements for js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+const homeScore = document.querySelector(".home-score");
+const guestScore = document.querySelector(".guest-score");
+
+const onePoint = document.getElementById("add-one-point");
+const twoPoints = document.getElementById("add-two-points");
+const threePoints = document.getElementById("add-three-points");
+
+let homeResult = 1;
+homeScore.textContent= homeResult;
+
+let guestResult = 5;
+guestScore.textContent= guestResult;
